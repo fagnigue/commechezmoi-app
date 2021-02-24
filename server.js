91bef3a8223a8da4166750fbb0dd6bfdb3fc33f9
fagnigue <fagniguecoulibaly@yahoo.com>
2021-02-24 09:36:05 +0000

    modif dans server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/commechezmoi-app'));
+app.use(express.static('./dist/CommeChezMoi2'));
 
 app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: 'dist/commechezmoi-app' }
