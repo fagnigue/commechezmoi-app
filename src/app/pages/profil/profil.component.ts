fc3262484d76d72bc3a7af337b3d35d343b49d81
fagnigue <fagniguecoulibaly@yahoo.com>
2021-02-28 14:26:21 +0000

    logout

--- src/app/pages/profil/profil.component.ts
+++ src/app/pages/profil/profil.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CookieService } from 'ngx-cookie';
+
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cookieService: CookieService, private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+
+
+  logout(){
+  	this.removecookie('infos');
+  	this.route.navigate(['/']);
+  }
+
+  removecookie(key: string) {
+  	this.cookieService.remove(key);
   }
 
 }
